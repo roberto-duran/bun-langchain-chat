2ed3128cb7bcb45e361b28530aef7cec0df74b62
Roberto <shaggyrd@gmail.com>
2023-09-09 15:11:16 -0500

    init drizzle

---
+++ app/src/api/auth/[…nextuth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db/schema";
+
+export default NextAuth({
+    adapter: DrizzleAdapter(db),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+    ],
+});
